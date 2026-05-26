c3715e8090edbe23f8830d89436f53df14ce358b
jstart0105 <jstartsolucoes@gmail.com>
2026-05-26 16:29:27 -0300

    Add files via upload

---
+++ PF-Wesley/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="header">
+        <img src="" alt="">
+    </div>
+</body>
+</html>
