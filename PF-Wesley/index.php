ccdf02ea17332426ae84da86469d69c8a2328b8f
jstart0105 <jstartsolucoes@gmail.com>
2026-06-01 09:28:07 -0300

    0.1

--- PF-Wesley/index.php
+++ PF-Wesley/index.php
@@ -3,11 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">     
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <div class="header">
-        <img src="" alt="">
-    </div>
+<body>  
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="" class="logo_container">
+            <img src="assets/logoHeader.png" alt="" class="logo_header">
+        </a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="">Recursos</a></li>
+          <li><a href="">Casos de Uso</a></li>
+          <li><a href="">Preços</a></li>
+          <li><a href="">Sobre Nós</a></li>
+          <li><a class="waves-effect waves-light btn">Acessar Portal</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </html>
